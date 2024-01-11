9f07b528287a86290cb0f15d57bd9655474023c1
Sam Atman <atmanistan@gmail.com>
2024-01-11 17:26:32 -0500

    Make icons a Dict

--- src/ToggleMenus.jl
+++ src/ToggleMenus.jl
@@ -6,7 +6,7 @@ using REPL.TerminalMenus
 
 mutable struct ToggleMenuMaker
     settings::Vector{Char}
-    icons::Vector{Char}
+    icons::Dict{Char,Char}
     pagesize::Int
     config::TerminalMenus.Config
     # probably a header template? we'll get there
@@ -15,11 +15,22 @@ end
 const StringVector = Vector{S} where S <: AbstractString
 
 function ToggleMenuMaker(settings::Vector{Char}, pagesize=10, kwargs...)
-    ToggleMenuMaker(settings, copy(settings), pagesize, TerminalMenus.Config(kwargs...))
+    icons = Dict{Char,Char}()
+    for char in settings
+        icons[char] = char
+    end
+    ToggleMenuMaker(settings, icons, pagesize, TerminalMenus.Config(kwargs...))
 end
 
 function ToggleMenuMaker(settings::Vector{Char}, icons::Vector{Char}, pagesize=10, kwargs...)
-    ToggleMenuMaker(settings, icons, pagesize, TerminalMenus.Config(kwargs...))
+    if length(settings) ≠ length(icons)
+        throw(DimensionMismatch("settings and icons must have the same number of elements"))
+    end
+    icodict = Dict{Char,Char}()
+    for (idx,char) ∈ settings |> enumerate
+        icodict[char] = icons[idx]
+    end
+    ToggleMenuMaker(settings, icodict, pagesize, TerminalMenus.Config(kwargs...))
 end
 
 makemenu(maker::ToggleMenuMaker, options::StringVector) = ToggleMenu(options, maker)
@@ -30,7 +41,7 @@ mutable struct ToggleMenu <: TerminalMenus._ConfiguredMenu{TerminalMenus.Config}
     pageoffset::Int
     settings::Vector{Char}
     selections::Vector{Char}
-    icons::Vector{Char}
+    icons::Dict{Char,Char}
     config::TerminalMenus.Config
 end
 
@@ -39,7 +50,10 @@ function ToggleMenu(options::StringVector,
                     selections::Vector{Char},
                     pagesize=10,
                     kwargs...)
-    icons = copy(settings)
+    icons = Dict{Char,Char}
+    for char in settings
+        icons[char] = char
+    end
     ToggleMenu(options, pagesize, 0, settings, selections, icons, TerminalMenus.Config(kwargs...))
 end
 
@@ -68,7 +82,7 @@ TerminalMenus.cancel(menu::ToggleMenu) = menu.selections = fill('\0', length(men
 TerminalMenus.numoptions(menu::ToggleMenu) = length(menu.options)
 
 function TerminalMenus.writeline(buf::IOBuffer, menu::ToggleMenu, idx::Int, ::Bool)
-    print(buf, '[', menu.selections[idx], ']', ' ')
+    print(buf, '[', menu.icons[menu.selections[idx]], ']', ' ')
     print(buf, replace(menu.options[idx], "\n" => "\\n"))
 end
 
