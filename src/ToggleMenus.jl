b8a7883409e219361d1986f1d71c4819cdb4f404
Sam Atman <atmanistan@gmail.com>
2024-01-11 17:07:43 -0500

    Add ToggleMenuMaker
    My use case will want to reuse everything but the options, so this
    approach is sensible.

--- src/ToggleMenus.jl
+++ src/ToggleMenus.jl
@@ -1,36 +1,60 @@
 module ToggleMenus
 
-export ToggleMenu
+export ToggleMenu, ToggleMenuMaker, makemenu
 
 using REPL.TerminalMenus
 
+mutable struct ToggleMenuMaker
+    settings::Vector{Char}
+    icons::Vector{Char}
+    pagesize::Int
+    config::TerminalMenus.Config
+    # probably a header template? we'll get there
+end
+
+const StringVector = Vector{S} where S <: AbstractString
+
+function ToggleMenuMaker(settings::Vector{Char}, pagesize=10, kwargs...)
+    ToggleMenuMaker(settings, copy(settings), pagesize, TerminalMenus.Config(kwargs...))
+end
+
+function ToggleMenuMaker(settings::Vector{Char}, icons::Vector{Char}, pagesize=10, kwargs...)
+    ToggleMenuMaker(settings, icons, pagesize, TerminalMenus.Config(kwargs...))
+end
+
+makemenu(maker::ToggleMenuMaker, options::StringVector) = ToggleMenu(options, maker)
+
 mutable struct ToggleMenu <: TerminalMenus._ConfiguredMenu{TerminalMenus.Config}
-    options::Vector{S} where S <: AbstractString
+    options::StringVector
     pagesize::Int
     pageoffset::Int
     settings::Vector{Char}
     selections::Vector{Char}
-    initial::Vector{Char}
     icons::Vector{Char}
     config::TerminalMenus.Config
 end
 
-function ToggleMenu(options::Vector{S} where S <: AbstractString,
+function ToggleMenu(options::StringVector,
                     settings::Vector{Char},
-                    initial::Vector{Char},
+                    selections::Vector{Char},
                     pagesize=10,
                     kwargs...)
-    selections = copy(initial)
     icons = copy(settings)
-    ToggleMenu(options, pagesize, 0, settings, selections, initial, icons, TerminalMenus.Config(kwargs...))
+    ToggleMenu(options, pagesize, 0, settings, selections, icons, TerminalMenus.Config(kwargs...))
 end
 
-function ToggleMenu(options::Vector{S} where S <: AbstractString,
+function ToggleMenu(options::StringVector,
                     settings::Vector{Char},
                     pagesize=10,
                     kwargs...)
-    initial = fill(settings[1], length(options))
-    ToggleMenu(options, settings, initial, pagesize, kwargs...)
+
+    selections = fill(settings[1], length(options))
+    ToggleMenu(options, settings, selections, pagesize, kwargs...)
+end
+
+function ToggleMenu(options::StringVector, maker::ToggleMenuMaker)
+    selections = fill(maker.settings[1], length(options))
+    ToggleMenu(options, maker.pagesize, 0, maker.settings, selections, maker.icons, maker.config)
 end
 
 function TerminalMenus.header(menu::ToggleMenu)
@@ -39,11 +63,11 @@ end
 
 TerminalMenus.pick(::ToggleMenu, ::Int)::Bool = true
 
-TerminalMenus.cancel(menu::ToggleMenu) = menu.selections = Vector{Char}('\0', length(menu.options))
+TerminalMenus.cancel(menu::ToggleMenu) = menu.selections = fill('\0', length(menu.options))
 
 TerminalMenus.numoptions(menu::ToggleMenu) = length(menu.options)
 
-function TerminalMenus.writeline(buf::IOBuffer, menu::ToggleMenu, idx::Int, cursor::Bool)
+function TerminalMenus.writeline(buf::IOBuffer, menu::ToggleMenu, idx::Int, ::Bool)
     print(buf, '[', menu.selections[idx], ']', ' ')
     print(buf, replace(menu.options[idx], "\n" => "\\n"))
 end
