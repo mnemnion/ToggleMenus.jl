8a38ccfba4fd0fd47e58133eb2085a835229b25c
Sam Atman <atmanistan@gmail.com>
2024-03-14 18:30:37 -0400

    Add ToggleMenus docstring

--- src/ToggleMenus.jl
+++ src/ToggleMenus.jl
@@ -1,3 +1,14 @@
+# SPDX-License-Identifier: MIT
+
+"""
+    ToggleMenus
+
+The `ToggleMenus` module provides a TerminalMenu where options may be toggled through
+several states.  A template is constructed with a [`ToggleMenuMaker`](@ref), which is
+used to construct a [`ToggleMenu`](@ref).  The result is passed to
+[`TerminalMenus.request`](@extref `REPL.TerminalMenus.request`) for display in the
+REPL.
+"""
 module ToggleMenus
 
 export ToggleMenu, ToggleMenuMaker
