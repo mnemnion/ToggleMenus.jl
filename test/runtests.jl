9c1d772b33fe295720ed8d6e26102778138dc65d
Sam Atman <atmanistan@gmail.com>
2024-03-17 10:58:12 -0400

    Test user functions + activate Aqua
    Ship it.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ page_up!, pick, printmenu, scroll_wrap, selected, writeline, didcancelmenu
 
 @testset "ToggleMenus.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        # Aqua.test_all(ToggleMenus)
+        Aqua.test_all(ToggleMenus)
     end
     @testset "ToggleMenuMaker errors" begin
         settings = ['a', 'b', 'c']
@@ -30,12 +30,13 @@ page_up!, pick, printmenu, scroll_wrap, selected, writeline, didcancelmenu
         @test menu1.selections == ['a', 'a', 'a']
         @test menu1.cursor[] == 1
         @test simpletemplate.header ==  "test menu 1"
-        template = ToggleMenuMaker("header", ['a', 'b', 'c'], ['🔴', '🔵', '🟢'], charset=:unicode)
+        template = ToggleMenuMaker("header", ['a', 'b', 'c'], ['🔴', '🔵', '🟢'], 7, charset=:unicode)
         menu = template("different header", [string(c) for c in 'a':'g'])
         @test template.header == "header"
         @test menu.header == "different header"
         @test menu.cursor[] == 1
         @test menu.cursor isa Base.RefValue{Int64}
+        @test menu.pagesize == 7
         @test (menu.cursor[] = move_down!(menu, menu.cursor[])) == 2
         @test (menu.cursor[] = move_up!(menu, menu.cursor[])) == 1
         @test (menu.cursor[] = page_down!(menu, menu.cursor[])) == 7
@@ -65,11 +66,11 @@ page_up!, pick, printmenu, scroll_wrap, selected, writeline, didcancelmenu
         @test didcancelmenu(cancel_return) == true
     end
     @testset "Menu with inert lines" begin
-        options = [string(c)^3 for c in 'a':'t']
+        options = [string(c)^3 for c in 'a':'u']
         settings = ['a', 'b']
         icons = ['🟢', '🔵']
-        selections = [i % 4 == 0 ? 'a' : '\0' for i in 1:20]
-        template = ToggleMenuMaker("with gaps", settings, icons, scroll_wrap=true)
+        selections = [i % 4 == 0 ? 'a' : '\0' for i in 1:21]
+        template = ToggleMenuMaker("with gaps", settings, icons; scroll_wrap=true)
         menu = template(options, selections)
         @test menu.cursor[] == 4
         @test (menu.cursor[] = move_down!(menu, menu.cursor[])) == 8
@@ -80,5 +81,26 @@ page_up!, pick, printmenu, scroll_wrap, selected, writeline, didcancelmenu
         @test (menu.cursor[] = move_down!(menu, menu.cursor[])) == 4
         @test menu.selections[menu.cursor[]] == 'a'
         @test scroll_wrap(menu) == true
+    end
+    @testset "User Functions" begin
+        settings = ['a', 'b', 'c']
+        header_fn(m) = string(m.selections)
+        function onkey(m, i)
+            if Char(i) == 'F'
+                m.selections = fill('c', numoptions(m))
+            end
+            return false
+        end
+        template = ToggleMenuMaker(header_fn, settings; keypress=onkey)
+        options = [string(c)^3 for c in 'a':'z']
+        selections = fill('a', length(options))
+        menu = template(options, selections)
+        @test menu.header isa Function
+        @test header(menu) == "['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']"
+        keypress(menu, UInt32('F'))
+        @test header(menu) == "['c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c']"
+        io = IOBuffer()
+        writeline(io, menu, menu.cursor[], true)
+        @test String(take!(io)) == "[c] aaa"
     end
 end
